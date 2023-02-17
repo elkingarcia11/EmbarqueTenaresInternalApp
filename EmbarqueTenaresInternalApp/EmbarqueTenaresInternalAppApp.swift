673638de33e3a826975c7877fc9713bb16193887
Elkin Garcia <elkingarcia.11@gmail.com>
2023-02-17 17:09:30 -0500

    Initial Commit

---
+++ EmbarqueTenaresInternalApp/EmbarqueTenaresInternalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmbarqueTenaresInternalAppApp.swift
+//  EmbarqueTenaresInternalApp
+//
+//  Created by Elkin Garcia on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EmbarqueTenaresInternalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
